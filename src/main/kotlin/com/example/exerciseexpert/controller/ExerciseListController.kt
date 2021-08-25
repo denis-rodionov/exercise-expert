3874121ba99c1c99cde368d68c5a6d855f8680fc
denis-rodionov <den.iu7@gmail.com>
2021-08-26 00:04:02 +0300

    delete exercise

--- src/main/kotlin/com/example/exerciseexpert/controller/ExerciseListController.kt
+++ src/main/kotlin/com/example/exerciseexpert/controller/ExerciseListController.kt
@@ -5,16 +5,12 @@ import com.example.exerciseexpert.domain.UserContext
 import com.example.exerciseexpert.domain.UserName
 import com.example.exerciseexpert.form.CreateExercise
 import com.example.exerciseexpert.repository.ExerciseRepository
-import com.example.exerciseexpert.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.Errors
-import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import javax.validation.Valid
 
 
@@ -28,7 +24,6 @@ class ExerciseListController : BaseController() {
     fun getAllExercises(@ModelAttribute userContext: UserContext, model: Model): String {
         val allExercises = exerciseRepository.findAll()
         model.addAttribute("exercises", allExercises)
-
         return "exercise-list"
     }
 
@@ -45,18 +40,22 @@ class ExerciseListController : BaseController() {
                        @ModelAttribute userContext: UserContext,): String {
         logger.info("DEBUG: creating exercise...")
         if (errors.hasErrors()) {
-            logger.info("DEBUG: errors found")
             return "create-exercise"
         }
         logger.info("User ${userContext.user} created exercise")
 
-        //val author = userContext.user!!
         userContext.user?.let { author ->
             val exercise = Exercise(null, newExercise.name!!,
                 UserName(null, author.name, author.id!!), author.name)
             exerciseRepository.save(exercise)
         } ?: throw Exception("Current user is not known")
 
+        return "redirect:/exercise"
+    }
+
+    @GetMapping("delete/{id}")
+    fun deleteExercise(@PathVariable("id") exerciseId: String, model: Model): String? {
+        exerciseRepository.deleteById(exerciseId);
         return "redirect:/exercise"
     }
 }
