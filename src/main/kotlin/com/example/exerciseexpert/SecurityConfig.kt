d1bbeacb080b74974cfb2406ea16ddb207233065
denis-rodionov <den.iu7@gmail.com>
2021-08-26 01:15:23 +0300

    allow css for unregistered users

--- src/main/kotlin/com/example/exerciseexpert/SecurityConfig.kt
+++ src/main/kotlin/com/example/exerciseexpert/SecurityConfig.kt
@@ -2,10 +2,12 @@ package com.example.exerciseexpert
 
 import com.example.exerciseexpert.service.AuthService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
@@ -25,7 +27,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
-            .antMatchers("/auth/register", "/login").permitAll()
+            .antMatchers("/auth/register", "/login", "/*.css").permitAll()
             .anyRequest().authenticated()
             .and().formLogin().loginPage("/login")
             .loginProcessingUrl("/login")
