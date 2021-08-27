984c0af56466e3563510681780eb1b29cc7dd6f6
denis-rodionov <den.iu7@gmail.com>
2021-08-27 23:27:42 +0300

    exercise edit

--- src/main/kotlin/com/example/exerciseexpert/controller/ExerciseListController.kt
+++ src/main/kotlin/com/example/exerciseexpert/controller/ExerciseListController.kt
@@ -1,16 +1,16 @@
 package com.example.exerciseexpert.controller
 
 import com.example.exerciseexpert.domain.Exercise
+import com.example.exerciseexpert.domain.User
 import com.example.exerciseexpert.domain.UserContext
 import com.example.exerciseexpert.domain.UserName
-import com.example.exerciseexpert.form.CreateExercise
+import com.example.exerciseexpert.form.ExerciseForm
 import com.example.exerciseexpert.repository.ExerciseRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.Errors
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import javax.validation.Valid
 
 
@@ -29,33 +29,56 @@ class ExerciseListController : BaseController() {
 
     @GetMapping("/create")
     fun createExerciseForm(@ModelAttribute userContext: UserContext, model: Model): String {
-        model.addAttribute("newExercise", CreateExercise())
-        return "create-exercise"
+        model.addAttribute("exercise", ExerciseForm())
+        return "exercise-view"
     }
 
     @PostMapping
-    fun createExercise(@ModelAttribute(name = "newExercise") @Valid newExercise: CreateExercise,
+    fun saveExercise(@ModelAttribute(name = "exercise") @Valid exercise: ExerciseForm,
                        errors: Errors,
                        model: Model,
                        @ModelAttribute userContext: UserContext,): String {
-        logger.info("DEBUG: creating exercise...")
+        logger.info("DEBUG: save exercise $exercise")
         if (errors.hasErrors()) {
-            return "create-exercise"
+            return "exercise-view"
         }
-        logger.info("User ${userContext.user} created exercise")
+        logger.info("User ${userContext.user} saving exercise...")
 
-        userContext.user?.let { author ->
-            val exercise = Exercise(null, newExercise.name!!,
+        val author: User =  userContext.user ?: throw Exception("User not found")
+
+        if (exercise.new) {
+            val newExercise = Exercise(null, exercise.name!!,
                 UserName(null, author.name, author.id!!), author.name)
-            exerciseRepository.save(exercise)
-        } ?: throw Exception("Current user is not known")
+            exerciseRepository.save(newExercise)
+        } else {
+            var exerciseId = exercise.id ?: throw Exception("Exercise ID not found")
+            val existingExercise = exerciseRepository.findById(exerciseId)
+                .orElseThrow()
+            logger.info(existingExercise.toString())
+            existingExercise.name = exercise.name!!
+            exerciseRepository.save(existingExercise)
+        }
 
         return "redirect:/exercise"
     }
 
     @GetMapping("delete/{id}")
-    fun deleteExercise(@PathVariable("id") exerciseId: String, model: Model): String? {
-        exerciseRepository.deleteById(exerciseId);
+    fun deleteExercise(@PathVariable("id") exerciseId: String): String {
+        exerciseRepository.deleteById(exerciseId)
         return "redirect:/exercise"
+    }
+
+    @GetMapping("edit/{id}")
+    fun showEditExercisePage(@PathVariable("id") exerciseId: String, model: Model): String {
+        logger.info("Edit exercise $exerciseId")
+        val exercise = exerciseRepository.findById(exerciseId).orElseThrow()
+
+        var exerciseForm = ExerciseForm(
+            name = exercise.name,
+            new = false,
+            id = exerciseId
+        )
+        model.addAttribute("exercise", exerciseForm)
+        return "exercise-view"
     }
 }
--- src/main/kotlin/com/example/exerciseexpert/form/ExerciseForm.kt
+++ src/main/kotlin/com/example/exerciseexpert/form/ExerciseForm.kt
@@ -2,7 +2,11 @@ package com.example.exerciseexpert.form
 
 import javax.validation.constraints.NotBlank
 
-data class CreateExercise(
+data class ExerciseForm(
     @field:NotBlank(message = "name should not be empty")
     var name: String? = null,
+
+    var new: Boolean = true,
+
+    var id: String? = null
 )
