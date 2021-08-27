d5edb8c685c96186772f72a06929cbbb4e2c37e0
denis-rodionov <den.iu7@gmail.com>
2021-08-28 02:42:17 +0300

    user deletion added

--- src/main/kotlin/com/example/exerciseexpert/controller/UserController.kt
+++ src/main/kotlin/com/example/exerciseexpert/controller/UserController.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Repository
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -20,5 +21,12 @@ class UserController: BaseController() {
         val users = userRepository.findAll()
         model.addAttribute("users", users)
         return "user-list"
+    }
+
+    @GetMapping("delete/{id}")
+    fun deleteExercise(@PathVariable("id") userId: String): String {
+        logger.info("DEBUG: deleting user with ID=$userId")
+        userRepository.deleteById(userId)
+        return "redirect:/user"
     }
 }
